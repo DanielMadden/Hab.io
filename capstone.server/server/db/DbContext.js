c8e8f091d625755334b087e8b84deb375141428c
Tim Zaleski <zaleskitimothy@yahoo.com>
2021-02-04 15:50:52 -0700

    messages added

--- capstone.server/server/db/DbContext.js
+++ capstone.server/server/db/DbContext.js
@@ -3,12 +3,14 @@ import ValueSchema from '../models/Value'
 import AccountSchema from '../models/Account'
 import GroupSchema from '../models/Group'
 import BadgeSchema from '../models/Badge'
+import MessageSchema from '../models/Message'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Groups = mongoose.model('Group', GroupSchema);
   Badges = mongoose.model('Badge', BadgeSchema);
+  Messages = mongoose.model('Message', MessageSchema);
 }
 
 export const dbContext = new DbContext()
