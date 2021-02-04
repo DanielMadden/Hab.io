c2321e3c001fc8d98d5858a583b3842b69c9b845
Daniel Madden <danielfpjobs@gmail.com>
2021-02-04 10:36:20 -0700

    Minor adjustments

--- capstone.client/src/pages/HomePage.vue
+++ capstone.client/src/pages/HomePage.vue
@@ -1,6 +1,7 @@
 <template>
   <div
-    class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center"
+    id="home"
+    class="container-fluid d-flex flex-column align-items-center justify-content-center"
   >
     This is a homepage
   </div>
@@ -13,12 +14,12 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home {
-  text-align: center;
-  user-select: none;
-  > img {
-    height: 200px;
-    width: 200px;
-  }
-}
+// .home {
+//   text-align: center;
+//   user-select: none;
+//   > img {
+//     height: 200px;
+//     width: 200px;
+//   }
+// }
 </style>
