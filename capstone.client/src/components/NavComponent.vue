a8147b8ab5d6db94bb9123d05b278ef63fcabd8b
Daniel Madden <danielfpjobs@gmail.com>
2021-02-05 11:53:57 -0700

    NavBar has been framed

--- capstone.client/src/components/NavComponent.vue
+++ capstone.client/src/components/NavComponent.vue
@@ -1,16 +1,24 @@
 <template lang="">
-  <div id="navbar" class="container-fluid">
-    <div id="navbar-vert-stretch" class="row">
+  <div id="nav-bar" class="container-fluid">
+    <div id="nav-vert-stretch" class="row">
       <div class="col-6 d-flex justify-content-start align-items-center">
         <h1>Logo</h1>
       </div>
       <div class="col-6 d-flex justify-content-end align-items-center">
-        <h1 v-if="!user.isAuthenticated">
+        <button v-if="!user.isAuthenticated" @click="login">
           Log In
-        </h1>
-        <button v-if="user.isAuthenticated">
+        </button>
+        <button v-if="user.isAuthenticated" @click="logout">
+          Log Out
+        </button>
+        <button v-if="user.isAuthenticated" @click="showGroups">
           My Groups
         </button>
+        <img v-if="user.isAuthenticated"
+             @click="showGroups"
+             :src="user.picture"
+             id="nav-profile-image"
+        />
       </div>
     </div>
   </div>
@@ -18,12 +26,15 @@
 <script>
 import { computed } from 'vue'
 import { AppState } from '../AppState'
+import { AuthService } from '../services/AuthService'
 export default {
   setup() {
     const account = computed(() => AppState.account)
     const user = computed(() => AppState.user)
+    const login = () => { AuthService.loginWithPopup() }
+    const logout = async() => { await AuthService.logout({ returnTo: window.location.origin }) }
     return {
-      account, user
+      account, user, login, logout
     }
   }
 }
