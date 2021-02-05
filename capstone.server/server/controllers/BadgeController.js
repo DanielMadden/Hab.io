b9bb1480de7d475d81fbc7797925500bc1dbf162
trevor-r-allen <tallen2699@gmail.com>
2021-02-05 14:45:03 -0700

    added getAll badges

--- capstone.server/server/controllers/BadgeController.js
+++ capstone.server/server/controllers/BadgeController.js
@@ -6,8 +6,18 @@ export class BadgeController extends BaseController {
   constructor() {
     super('api/badges')
     this.router
+      .get('', this.getAll)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.create)
+  }
+
+  async getAll(req, res, next) {
+    try {
+      const data = await badgeService.getAll()
+      res.send(data)
+    } catch (error) {
+      next(error)
+    }
   }
 
   async create(req, res, next) {
--- capstone.server/server/services/BadgeService.js
+++ capstone.server/server/services/BadgeService.js
@@ -2,6 +2,10 @@ import { dbContext } from '../db/DbContext'
 // import { BadRequest } from '../utils/Errors'
 
 class BadgeService {
+  async getAll() {
+    return await dbContext.Badges
+  }
+
   async create(body) {
     return await dbContext.Badges.create(body)
   }
