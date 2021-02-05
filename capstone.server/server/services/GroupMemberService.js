6ea4504dc23db8942326484d83ecdfd15a1fb558
trevor-r-allen <tallen2699@gmail.com>
2021-02-04 17:35:17 -0700

    bugfix on groupmember delete

--- capstone.server/server/services/GroupMemberService.js
+++ capstone.server/server/services/GroupMemberService.js
@@ -2,25 +2,26 @@ import { dbContext } from "../db/DbContext";
 import { BadRequest } from "../utils/Errors";
 
 class GroupMemberService {
-  getGroupMembersByGroupId(groupId) {
+  async getGroupMembersByGroupId(groupId) {
     return dbContext.GroupMembers.find({groupId: groupId}).populate('memberId')
     
   }
   async create(groupMember) {
     return await dbContext.GroupMembers.create(groupMember)
   }
-  edit(groupMemberId, update, accountId) {
+  async edit(groupMemberId, update, accountId) {
     const groupMember = dbContext.GroupMembers.findById(groupMemberId)
     if(groupMember.status === 'Moderator'){
       return dbContext.GroupMembers.findOneAndUpdate(groupMemberId, update)
     }
   }
-  delete(groupMemberId, accountId) {
+  async delete(groupMemberId, accountId) {
     // if not working put a breakpoint and hover over the variables
-    const groupMember = dbContext.GroupMembers.findById(groupMemberId)
-    const group = dbContext.Groups.findById(groupMember.groupId)
+    const groupMember = await (await dbContext.GroupMembers.findById(groupMemberId)).toObject()
+    const group = await (await dbContext.Groups.findById(groupMember.groupId)).toObject()
+    const currentGroupMember = await (await dbContext.GroupMembers.findOne({memberId: accountId, groupId: groupMember.id})).toObject()
     if(group.creatorId !== accountId){
-      if(groupMember.memberId === accountId || groupMember.status === 'Moderator'){
+      if(groupMember.memberId === accountId || currentGroupMember.status === 'Moderator'){
         return dbContext.GroupMembers.findOneAndRemove(groupMemberId)
       }
       else{
