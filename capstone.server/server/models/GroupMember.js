a17f68ced8701b5cb12fe18387d3b59c923f52a4
trevor-r-allen <tallen2699@gmail.com>
2021-02-08 12:16:37 -0700

    reverted group property on groupmember

--- capstone.server/server/models/GroupMember.js
+++ capstone.server/server/models/GroupMember.js
@@ -4,8 +4,7 @@ const ObjectId = mongoose.Schema.Types.ObjectId
 
 const GroupMember = new Schema(
   {
-    groupId: { type: String, required: true },
-    group: { type: ObjectId, ref: 'Group', required: true },
+    groupId: { type: ObjectId, ref: 'Group', required: true },
     memberId: { type: String, ref: 'Account', required: true },
     status: { type: String, required: true }
   }, { timestamps: true, toJSON: { virtuals: true } }
