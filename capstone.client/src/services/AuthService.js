7d26dabe5ada2f9677169ca9b06d90af4317d79e
trevor-r-allen <tallen2699@gmail.com>
2021-02-08 11:34:45 -0700

    bug fix

--- capstone.client/src/services/AuthService.js
+++ capstone.client/src/services/AuthService.js
@@ -5,7 +5,6 @@ import router from '../router'
 import { setBearer } from './AxiosService'
 import { accountService } from './AccountService'
 import { groupService } from './GroupService'
-import { groupMemberService } from './GroupMemberService'
 
 export const AuthService = initialize({
   domain,
@@ -26,5 +25,4 @@ AuthService.on(AuthService.AUTH_EVENTS.AUTHENTICATED, async function() {
   AppState.user = AuthService.user
   // NOTE if there is something you want to do once the user is authenticated, place that here
   groupService.getAccountGroups(AppState.account.id, true)
-  groupMemberService.getGroupMembers(AppState.account.id)
 })
