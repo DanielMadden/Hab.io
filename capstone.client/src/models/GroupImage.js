327e3fb557a5fe0ebcf8594776154b0e4de94909
Tim Zaleski <zaleskitimothy@yahoo.com>
2021-02-08 15:13:06 -0700

    higher res images

--- capstone.client/src/models/GroupImage.js
+++ capstone.client/src/models/GroupImage.js
@@ -1,13 +1,13 @@
 export default class GroupImage {
   constructor(data) {
     this.name = data.alt_description
-    this.imageURL = modifyURL(data.urls.small)
+    this.imageURL = modifyURL(data.urls.regular)
     this.photohrapher = data.user.name
     this.color = data.color
   }
 }
 function modifyURL(url) {
-  url = url.slice(0, -8)
-  url += '100&w=400'
+  url = url.slice(0, -9)
+  url += '100&w=1080'
   return url
 }
