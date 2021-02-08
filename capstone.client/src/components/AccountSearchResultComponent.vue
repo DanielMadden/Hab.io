bc9f9e3b1383707e89b73bbb6b09021adb06ddad
trevor-r-allen <tallen2699@gmail.com>
2021-02-08 16:29:06 -0700

    added search result component layout

---
+++ capstone.client/src/components/AccountSearchResultComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="account-compact d-flex">
+    <div
+      class="account-compact-image"
+      :style="`background: linear-gradient( rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.0)), url('${accountProp.memberId.picture}') no-repeat center center /cover; overflow-y: hidden`"
+    ></div>
+    <div class="account-compact-text d-flex flex-column">
+      <span>{{ accountProp.memberId.name }}</span>
+      <span v-if="accountProp.status === 'Moderator'">Moderator</span>
+      <span v-if="accountProp.status === 'Member'">Member</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    accountProp: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+<style scoped>
+@import "../assets/css/groupMember.css";
+</style>
