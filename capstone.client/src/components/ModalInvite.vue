38daf8335fc9d2fc5218866c7a32d45b002078c1
trevor-r-allen <tallen2699@gmail.com>
2021-02-08 16:24:57 -0700

    invite modal progress

--- capstone.client/src/components/ModalInvite.vue
+++ capstone.client/src/components/ModalInvite.vue
@@ -2,50 +2,63 @@
   <div class="myModal-content">
     <div class="myModal-frame dark-scrollbar">
       <input type="text"
-             placeholder="Invite"
+             placeholder="Search by name or email"
              name="invitees"
              id="invitees"
              v-model="state.query"
              @change="getAccounts"
       >
     </div>
+    <div class="search-results">
+      <AccountSearchResultComponent :v-for="profile in state.searchResults" :key="profile.id" :profile-prop="profile" />
+    </div>
     <div class="myModal-footer">
       <button id="myModal-button-join-group"
               class="myModal-button"
-              @click="invite"
+              @click="sendInvites"
       >
-        Join Group
+        Invite Selected
       </button>
     </div>
   </div>
 </template>
 <script>
-import { reactive } from 'vue'
+import { computed, reactive } from 'vue'
 import { logger } from '../utils/Logger'
 import { accountService } from '../services/AccountService'
+import { groupMemberService } from '../services/GroupMemberService'
+import { AppState } from '../AppState'
 export default {
   setup() {
     const state = reactive({
-      query: ''
+      query: '',
+      searchResults: computed(() => AppState.accountSearchResults),
+      selectedInvitees: [],
+      activeGroup: computed(() => AppState.activeGroup)
 
     })
     return {
 
-      state
-      // async getAccounts(query) {
-      //   try {
-      //     await accountService.
-      //   } catch (error) {
-      //     logger.error(error)
-      //   }
-      // },
-      // async sendInvite() {
-      //   try {
-
-      //   } catch (error) {
-      //     logger.error(error)
-      //   }
-      // }
+      state,
+      async getAccounts() {
+        try {
+          await accountService.getAccountsByQuery(state.query)
+        } catch (error) {
+          logger.error(error)
+        }
+      },
+      async sendInvites() {
+        state.selectedInvitees.forEach(invitee => {
+          this.sendInvite(invitee.id, state.activeGroup.id)
+        })
+      },
+      async sendInvite(inviteeId, groupId) {
+        try {
+          await groupMemberService.sendGroupInvite(inviteeId, groupId)
+        } catch (error) {
+          logger.error(error)
+        }
+      }
     }
   }
 }
