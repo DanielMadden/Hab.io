b14b41eac6869a840c9bfa57310551f2a4d18c89
trevor-r-allen <tallen2699@gmail.com>
2021-02-09 14:07:19 -0700

    Added will to group member

--- capstone.client/src/components/GroupMemberComponent.vue
+++ capstone.client/src/components/GroupMemberComponent.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="group-member d-flex">
-    <div
-      class="group-member-image"
-      :style="`background: linear-gradient( rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.0)), url('${groupMember.memberId.picture}') no-repeat center center /cover; overflow-y: hidden`"
-    ></div>
-    <div class="group-member-text d-flex flex-column">
-      <span>{{ groupMember.memberId.name }}</span>
-      <span v-if="groupMember.status === 'Moderator'">Moderator</span>
-      <span v-if="groupMember.status === 'Member'">Member</span>
+  <div class="row group-member">
+    <div class="col  d-flex">
+      <div
+        class="group-member-image"
+        :style="`background: linear-gradient( rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.0)), url('${groupMember.memberId.picture}') no-repeat center center /cover; overflow-y: hidden`"
+      ></div>
+      <div class="group-member-text d-flex flex-column">
+        <span>{{ groupMember.memberId.name }}</span>
+        <span>{{ groupMember.status }}</span>
+      </div>
+    </div>
+    <!-- TODO Display level instead of will -->
+    <div class="col d-flex justify-content-end align-items-center">
+      <span>Will - {{ groupMember.memberId.will }}</span>
     </div>
   </div>
 </template>
